fe4f70698d6f04fdfdfc827f3e9891c912d5c83b
chickenpop <1005honeybadger@gmail.com>
2022-07-05 00:14:06 +0900

    fix sql
    oracle 계정 및 새로운 테이블의 sql로 변경

--- src/main/java/com/study/gallery/DAO.java
+++ src/main/java/com/study/gallery/DAO.java
@@ -17,6 +17,34 @@ public class DAO {
 	public DAO() {
 		conn = DBUtil.open();
 	}
+
+	public int add(DTO dto) {
+		
+		System.out.println("add dto");
+		
+		try {
+			
+			Connection conn = null;
+			PreparedStatement pstat = null;
+			
+			conn = DBUtil.open();
+			
+			String sql = "INSERT INTO tblGallery (seq, filename, content, regdate) values (GALLERYSEQ.nextval, ?, ?, default)";
+			
+			pstat = conn.prepareStatement(sql);
+			
+			pstat.setString(1, dto.getFilename());
+			pstat.setString(2, dto.getContent());
+			
+			return pstat.executeUpdate();
+			
+		} catch (Exception e) {
+			System.out.println("DAO.add");
+			e.printStackTrace();
+		}
+		
+		return 0;
+	}
 	
 	
 	
